c5e5613feda6658b4233b2af5d0d0a8c7b99567b
JoelWiegand <16wiegandjr@gmail.com>
2021-08-24 11:43:57 -0500

    add methods

---
+++ Methods.java
@@ -0,0 +1,17 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = Integer.MIN_VALUE;
+		System.out.println(i);
+		
+		Double dubobject = 2.4;
+		double dubs=dubobject;
+		System.out.println(dubobject);
+		System.out.println(dubs);
+	}	
+	private static int minValue() {
+		return Integer.MIN_VALUE;
+	}
+}
--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 /**
- * helloworld
+ * hello world
  */
 public class helloworld {
     public static void main(String[] args) {
         System.out.print("Hello Cruel World!");
+        for (String string : args) {
+			System.out.println(string);
+		}
+        
+       Scanner s  = new Scanner(System.in);
     }
+
 }
