ed3487a6bdb3405f815fea8baa14b148d7e3b148
JoelWiegand <16wiegandjr@gmail.com>
2021-08-25 14:41:15 -0500

    m

--- Methods.java
+++ Methods.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 
 public class Methods {
 
@@ -8,10 +9,17 @@ public class Methods {
 		
 		Double dubobject = 2.4;
 		double dubs=dubobject;
+		double dubs2= Double.valueOf(dubobject);
 		System.out.println(dubobject);
 		System.out.println(dubs);
+		
+		String s= "ssvads";
+		for (int i2 =0;s.length()<i2;i2++) {
+			
+		}
 	}	
 	private static int minValue() {
 		return Integer.MIN_VALUE;
 	}
+	
 }
